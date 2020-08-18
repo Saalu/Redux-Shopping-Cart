9b51634fb9e6e82566ef8bded626c19d5cac1a31
unknown <saaluissaka>
2020-08-18 09:39:06 +0000

    cart component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Products from './components/Products';
 import data from './data.json';
 import Filter from './components/Filter';
+import Cart from './components/Cart';
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -13,6 +14,29 @@ class App extends Component {
 			sort: ''
 		};
 	}
+
+	removeFromCart = (product) => {
+		const cartItems = this.state.cartItems.slice();
+		this.setState({
+			cartItems: cartItems.filter((x) => x._id !== product._id)
+		});
+	};
+
+	addToCart = (product) => {
+		console.log('add to cart please');
+		const cartItems = this.state.cartItems.slice();
+		let alreadyInCart = false;
+		cartItems.forEach((item) => {
+			if (item._id === product._id) {
+				item.count++;
+				alreadyInCart = true;
+			}
+		});
+		if (!alreadyInCart) {
+			cartItems.push({ ...product, count: 1 });
+		}
+		this.setState({ cartItems });
+	};
 
 	filterProducts = (e) => {
 		//filter
@@ -43,7 +67,7 @@ class App extends Component {
 		}));
 	};
 	render() {
-		const { products, size, sort } = this.state;
+		const { products, size, sort, cartItems } = this.state;
 		return (
 			<div className="grid-container">
 				<header>
@@ -60,9 +84,11 @@ class App extends Component {
 								filterProducts={this.filterProducts}
 								sortProducts={this.sortProducts}
 							/>
-							<Products products={products} />
+							<Products products={products} addToCart={this.addToCart} />
 						</div>
-						<div className="sidebar">This is the cart Items</div>
+						<div className="sidebar">
+							<Cart cartItems={cartItems} removeFromCart={this.removeFromCart} />
+						</div>
 					</div>
 				</main>
 				<footer>All right is reserved.</footer>
--- src/components/Products.js
+++ src/components/Products.js
@@ -7,7 +7,7 @@ class Products extends Component {
 			<div>
 				<ul className="products">
 					{this.props.products.map((product) => (
-						<li key={product.id}>
+						<li key={product._id}>
 							<div className="product">
 								<a href={'#' + product._id}>
 									<img src={product.image} alt={product.title} />
@@ -16,7 +16,7 @@ class Products extends Component {
 							</div>
 							<div className="product-price">
 								<div>{formatCurrency(product.price)}</div>
-								<button onclick={() => this.props.addToCart(product)} className="button primary">
+								<button onClick={() => this.props.addToCart(product)} className="button primary">
 									Add To Cart
 								</button>
 							</div>
